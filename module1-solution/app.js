e46a37861bd652bf5f56abaf8093d2df5be108d0
sunsplat <heeycutie@yahoo.com>
2016-09-19 00:50:39 -0400

    creating assignment 1

---
+++ module1-solution/app.js
@@ -0,0 +1,15 @@
+(function () {
+	'use strict';
+
+	angular.module('LunchCheck', [])
+
+	.controller('LunchCheckController', function ($scope) {
+		$scope.name = "";
+		$scope.lunchItemValue = 0;
+		$scope.message = "";
+
+		function totalLunchItems() {
+
+		}
+	}
+})();
